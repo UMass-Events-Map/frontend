747b7b4a633c2515d6bb49174a63cdff6d79824f
Nam Nguyen <namhnguyen0103@gmail.com>
2024-10-24 17:47:40 -0400

    Change tab bar color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,17 +2,13 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#7E2622",
         headerShown: false,
       }}>
       <Tabs.Screen
