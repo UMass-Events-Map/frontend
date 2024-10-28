47505d6833edd48e9c1bbffe510ca92687f23d0f
Nam Nguyen <namhnguyen0103@gmail.com>
2024-10-28 17:54:38 -0400

    Add heading style for list page

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function List() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
     
-    
-    
-      <Text> LIST VIEW</Text>
-    
-    
+      <Text style={styles.heading}>EVENT LIST</Text>
     
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    marginTop: 50
+  },
+  heading: {
+    flex: 1,
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#7E2622'
+  }
+});
