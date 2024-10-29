d494b056a9150ab6a2ea9ee9f0ae577397170254
Zhihua Zhu <zhuazhu.zz@gmail.com>
2024-10-28 20:48:45 -0400

    added supabase.ts, useEffect with fetch in list.tsx

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,12 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { Text, View } from "react-native";
+import { supabase } from "@/utils/supabase";
+
 
 export default function List() {
+  const [events, setEvents] = useState<any[] | null>(null);
+  
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data } = await supabase
+        .from('creators')
+        .select('*');
+      setEvents(data);
+    };
+    fetchEvents();
+
+    console.log(events)
+  
+  }, []);
+
+
+
   return (
     <View style={{ flex: 1 }}>
     
     
     
-      <Text> LIST VIEW</Text>
+      <Text> LIST VIEW  {events}</Text>
     
     
     
---
+++ utils/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://gdacboczcpnnnqfxkjsj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkYWNib2N6Y3Bubm5xZnhranNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MjQwMzIsImV4cCI6MjA0NTIwMDAzMn0.z9pvzajPfB_qPYmFMD-U-_hcdfarXF3Kq7WHG3OPz5c';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
