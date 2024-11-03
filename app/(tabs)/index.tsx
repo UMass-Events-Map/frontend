c3ecff12ed930fc5dea7a964dcaea1fa85b7f813
Zhihua Zhu <zhuazhu.zz@gmail.com>
2024-11-03 14:04:11 -0500

    added some buildings to map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,33 @@
 import MapComponent from "@/components/MapComponent";
 import { Text, View, StyleSheet } from "react-native";
-
+import { useState, useEffect } from "react";
+import { supabase } from '@/utils/supabase';
 
 export default function Index() { // Map Component (index as default entry point)
+  const [buildings, setBuildings] = useState<any[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      const { data: buildings, error } = await supabase
+        .from('buildings')
+        .select('*');
+      
+      if (error) {
+        console.error("Error fetching buildings:", error.message);
+        setError(error.message);
+      } else {
+        setBuildings(buildings);
+      }
+    };
+    fetchBuildings();
+  }, []);
+  
+  
   return (
     <View style={{ flex: 1 }}>
-
-      <MapComponent />
+      
+      <MapComponent buildings={buildings}/>
     </View>
   );
 }
--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -3,9 +3,23 @@ import { Platform, Text, StyleSheet, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'; // (react native maps library doesnt support web loading/viewing)
 // import { GOOGLE_MAPS_API_KEY } from '@env';
 
-export default function MapComponent() {
-    // If using web view to test viewing the map it won't work, you must launch Emulator or Connect Physical Device
+type Building = {
+    id: string;
+    name: string;
+    latitude: number;
+    longitude: number;
+  };
+  
+  type BuildingListProps = {
+    buildings: Building[] | null;
+  };
 
+export default function MapComponent({ buildings }: BuildingListProps) {
+    // If using web view to test viewing the map it won't work, you must launch Emulator or Connect Physical Device
+    if (!buildings) {
+        return <Text>No buildings available</Text>;
+    }
+    console.log("dajd")
     return (
         <View style={{ flex: 1 }}>
             <MapView
@@ -18,11 +32,15 @@ export default function MapComponent() {
                     longitudeDelta: 0.012,
                 }}
             >
-                <Marker
-                coordinate={{ latitude: 42.390309, longitude: -72.527682 }}
-                title="Marker Title"
-                description="Marker Description"
-                />
+                {buildings.map(building => ( 
+                    <Marker
+                        coordinate={{ latitude: building.latitude, longitude: building.longitude }}
+                        title={building.name}
+                        description="Marker Description"
+                    />
+
+                ))}
+                
             </MapView>
         </View>
     );
