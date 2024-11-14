ef542cac8b8f062351ed085177dfea9b42b7775a
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-11-14 13:07:29 -0500

    chore: update .gitignore and remove unused binary files

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,5 @@ declare module '@env' {
     export const SUPABASE_URL: string;
     export const SUPABASE_ANON_KEY: string;
     export const EXPO_PACKAGE_NAME: string;
+    export const GOOGLE_MAPS_API_KEY: string;
 }
