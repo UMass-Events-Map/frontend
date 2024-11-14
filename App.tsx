4de371f1d7b8d4097e42f59bb0fa6242ee9a51a2
Zhihua Zhu <133460136+zhuazhuzz@users.noreply.github.com>
2024-11-13 20:35:10 -0800

    update

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
-import './app/sheets';
-import {SheetProvider} from "react-native-actions-sheet";
 import RootLayout from './app/_layout';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 const App = () => {
   return (
