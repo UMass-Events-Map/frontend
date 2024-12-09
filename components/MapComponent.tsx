6f4a0a4c69b19d32f5107c9b4f1d85ab035f9296
Zhihua Zhu <133460136+zhuazhuzz@users.noreply.github.com>
2024-12-08 18:24:56 -0800

    force

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -105,6 +105,9 @@ export default function MapComponent() {
         provider={Platform.OS === "android" ? PROVIDER_GOOGLE : undefined}
         style={styles.map}
         region={amherstRegion}
+        showsMyLocationButton
+        showsCompass
+        showsUserLocation
       >
         {buildings?.map((building) => (
           <Marker
