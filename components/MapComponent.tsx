f07bcee0c5f7ad4f88bce66dc47bacf5e22a09d6
Zhihua Zhu <133460136+zhuazhuzz@users.noreply.github.com>
2024-12-08 16:57:29 -0800

    get user current location

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,8 +1,11 @@
 import React, { useCallback, useMemo, useRef, useState, memo } from 'react';
-import { Platform, Text, StyleSheet, View, Image, ScrollView} from 'react-native';
+import { Platform, Text, StyleSheet, View, Image, ScrollView, TouchableOpacity, Alert} from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import ActionSheet, { SheetManager, ActionSheetRef, useScrollHandlers } from 'react-native-actions-sheet';
 import { BuildingProp, Building } from '@/constants/Interfaces'
+
+import * as Location from 'expo-location';
+
 
 const amherstRegion = { // Data to have map focus in on Amherst Area on load
     latitude: 42.390309,
@@ -13,7 +16,7 @@ const amherstRegion = { // Data to have map focus in on Amherst Area on load
 
 export default function MapComponent({ buildings }: BuildingProp) {
     if (!buildings) return <Text>No buildings available</Text>;
-    
+
     const handleMarkerPress = (building: Building) => {
         SheetManager.show('mapaction-sheet', {
             payload: { value: building },
@@ -27,6 +30,9 @@ export default function MapComponent({ buildings }: BuildingProp) {
                 provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : undefined}
                 style={styles.map}
                 region={amherstRegion}
+                showsUserLocation
+                showsMyLocationButton={true}
+                showsCompass={true}
             >
                 {buildings.map((building) => (
                     <Marker
